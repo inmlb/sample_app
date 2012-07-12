8ee4f1144ad9af158970d3d58fcac26019fe27ee
Marc-Laurent BOUTIN <marc-laurent.boutin@idneuves.fr>
2012-07-12 15:37:36 +0200

    Mise a jour apres page 28 chapitre 6

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  # Retourne un titre propre à la page.               # Commentaire de documentation
+  def titre                                           # Définition de la méthode
+    base_titre = "Simple App du Tutoriel Ruby on Rails"  # Assignement de variable
+    if @titre.nil?                                    # Test booléen pour la nullité
+      base_titre                                      # Retour implicite
+    else
+      "#{base_titre} | #{@titre}"                     # Interpolation de chaine
+    end
+  end
 end
